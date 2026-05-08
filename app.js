d893bfce9aa3d558ee2e045e1288fd169620c7e3
alexiactrindade <alexiadacostatrindade@gmail.com.br>
2026-05-08 15:12:10 -0300

    Adicionando função de listagem

--- app.js
+++ app.js
@@ -14,3 +14,67 @@ const mensagemLista = document.getElementById("mensagem-lista")
 const botaoSalvar = document.getElementById("botao-salvar")
 const botaoCancelar = document.getElementById("botao-cancelar")
 const botaoCarregar = document.getElementById("botao-atualizar")
+
+async function carregarContatos() {
+
+  mensagemLista.textContent = "Carregando contatos..."
+  corpoTabela.textContent = ""
+
+  const contatos = await getContatoss()
+
+  contatos.forEach(contato => {
+
+    const linha = document.createElement("tr")
+    const tdFoto = document.createElement("td")
+    const imagem = document.createElement("img")
+    imagem.src = contato.foto
+    imagem.alt = contato.nome
+    tdFoto.appendChild(imagem)
+
+    const tdNome = document.createElement("td")
+    tdNome.textContent = contato.nome
+
+    const tdCelular = document.createElement("td")
+    tdCelular.textContent = contato.celular
+
+    const tdEmail = document.createElement("td")
+    tdEmail.textContent = contato.email
+
+    const tdEndereco = document.createElement("td")
+    tdEndereco.textContent = contato.endereco
+
+    const tdCidade = document.createElement("td")
+    tdCidade.textContent = contato.cidade
+
+    const tdAcoes = document.createElement("td")
+    const botaoEditar = document.createElement("button")
+
+    botaoEditar.textContent = "Editar"
+    botaoEditar.addEventListener("click", () => { preencherFormulario(contato)})
+
+    const botaoExcluir = document.createElement("button")
+    botaoExcluir.textContent = "Excluir"
+  
+    botaoExcluir.addEventListener("click", async () => {
+      const confirmar = confirm("Deseja deletar este contato?")
+
+      if (confirmar) {
+        await deletarContato(contato.id)
+        carregarContatos()
+      }
+    })
+
+    tdAcoes.appendChild(botaoEditar)
+    tdAcoes.appendChild(botaoExcluir)
+    linha.appendChild(tdFoto)
+    linha.appendChild(tdNome)
+    linha.appendChild(tdCelular)
+    linha.appendChild(tdEmail)
+    linha.appendChild(tdEndereco)
+    linha.appendChild(tdCidade)
+    linha.appendChild(tdAcoes)
+    corpoTabela.appendChild(linha)
+  })
+
+  mensagemLista.textContent = ""
+}
