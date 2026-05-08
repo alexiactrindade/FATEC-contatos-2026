a364879e24d7a94611ec8d1ec607551a3e7be74b
alexiactrindade <alexiadacostatrindade@gmail.com.br>
2026-05-08 14:46:52 -0300

    atribuindo variaveis através da função getElementById

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 
 import { getContatoss, criarContato, atualizarContato, deletarContato} from "./contatos.js"
+
+const campoId = document.getElementById("campo-id")
+const campoNome = document.getElementById("campo-nome")
+const campoCelular = document.getElementById("campo-celular")
+const campoEmail = document.getElementById("campo-email")
+const campoFoto = document.getElementById("campo-foto")
+const campoEndereco = document.getElementById("campo-endereco")
+const campoCidade = document.getElementById("campo-cidade")
